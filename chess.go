0f860ffbb030f6fb2e3b682849579c6f39af98e5
polypmer <exorable.ludos@gmail.com>
2016-07-02 14:56:09 +0200

    120 bit board

--- chess.go
+++ chess.go
@@ -13,23 +13,43 @@ import (
 
 //var Board [128]byte
 
+/*
+    ' rnbqkbnr\n'  #  20 - 29
+    ' pppppppp\n'  #  30 - 39
+    ' ........\n'  #  40 - 49
+    ' ........\n'  #  50 - 59
+    ' ........\n'  #  60 - 69
+    ' ........\n'  #  70 - 79
+    ' PPPPPPPP\n'  #  80 - 89
+    ' RNBQKBNR\n'  #  90 - 99
 
+
+*/
 type Board struct {
 	board []byte
+	A1, H1, A8, H8 int
 }
 
 func (b Board) String() string {
 	var printBoard string
 	
-	
-	/* Good for 64 byte board
+	fmt.Print(string(b.board))
+	//* Good for 64 byte board
 	for idx, _ := range b.board {
-		fmt.Print( ":", idx, "  ")
-		sq := idx + 1
-		if sq % 8 == 0{
+		if idx < 10 {
+			fmt.Print(":00", idx, "  ")
+		}
+		if idx < 100 && idx > 10{
+			fmt.Print( ":0", idx, "  ")
+		}
+		if idx > 99 {
+			fmt.Print(":", idx, "  ")
+		}
+		sq := idx 
+		if sq % 10 == 0 && sq != 0{
 			fmt.Print("\n")
 		}
-	}*/
+	}
 			
 	return printBoard
 }
@@ -54,7 +74,8 @@ func (b Board) squareAt(x, y int) (int, error) {
 }
 
 func NewBoard() Board {
-	b := make([]byte, 128)
+	b := make([]byte, 120)
+
 	/*for i := 0; i < 8; i +=2 {
 		row0 := i * 8
 		row1 := (i + 1) * 8
@@ -65,17 +86,14 @@ func NewBoard() Board {
 			}
 		}
 	}*/
-	
-	for i := 8; i <16; i++{
-		b[i] = '1'
-	}
-	
-	for i := 48; i<56; i++{
-		b[i] = '8'
-	}
-
+	fmt.Println("initializing board")
+	//fmt.Println("At 20-29", string(b[28]))
 	return Board{
 		board: b,
+		A1: 91,
+		A8: 21,
+		H1: 98,
+		H8: 28,
 	}
 }
 
@@ -88,4 +106,5 @@ func main() {
 	//fmt.Print(board.String())
 	//fmt.Println(string(board.board[1:8]))
 	board.String()
+	fmt.Println("At 20-29", string(board.board[31]))
 }
