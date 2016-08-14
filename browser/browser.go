3f31fa22e96afddd0bb5fd65d43d96f5cb26c5ab
polypmer <exorable.ludos@gmail.com>
2016-08-14 13:27:27 -0400

    cause what is go if not a slick error handlin machine

--- browser/browser.go
+++ browser/browser.go
@@ -10,12 +10,19 @@ func boardHandler(w http.ResponseWriter, r *http.Request) {
 	game := ghess.NewBoard()
 	fmt.Fprintf(w, game.String())
 	move := r.URL.Path[1:]
-	game.ParseMove(move)
+	e := game.ParseMove(move)
+	if e != nil {
+		fmt.Fprintf(w, e.Error()+"\n")
+	}
 	fmt.Fprintf(w, game.String())
 }
 
 func main() {
 	// Listen
+	// So ahndFunc takes a custom Handler
+	// Which is forcement takes into a reader and writer
+	// and then it will print whatever is written to the
+	// writer
 	http.HandleFunc("/", boardHandler)
 	http.ListenAndServe("0.0.0.0:8080", nil)
 }
