00dca1a7fd2fe9add69008ce9ac038a74e5c46fa
polypmer <exorable.ludos@gmail.com>
2016-08-14 13:18:57 -0400

    it begins

--- browser/browser.go
+++ browser/browser.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/polypmer/ghess"
+	"net/http"
 )
 
-func main () {
+func boardHandler(g *Board, w http.ResponseWriter, r *http.Request) {
+	game := ghess.NewBoard()
+	fmt.Fprintf(w, g.String())
+}
+
+func main() {
 	// Listen
+	http.HandleFunc("/", boardHandler)
+	http.ListenAndServe("0.0.0.0:8080", nil)
 }
