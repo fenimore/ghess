7ab841b2274ee057c98690cb4c81b4ca4056fb7c
polypmer <exorable.ludos@gmail.com>
2016-08-15 12:01:18 -0400

    ok... no sessions, rather use a database

--- browser/browser.go
+++ browser/browser.go
@@ -1,28 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/mattn/go-sqlite3"
 	"github.com/polypmer/ghess"
 	"net/http"
 )
 
 func boardHandler(w http.ResponseWriter, r *http.Request) {
 	game := ghess.NewBoard()
-	fmt.Fprintf(w, game.String())
+	fmt.Fprintln(w, game.String())
 	move := r.URL.Path[1:]
 	e := game.ParseMove(move)
 	if e != nil {
-		fmt.Fprintf(w, e.Error()+"\n")
+		fmt.Fprintln(w, e.Error())
 	}
-	fmt.Fprintf(w, game.String())
+	fmt.Fprintln(w, game.String())
 }
 
 func main() {
-	// Listen
-	// So ahndFunc takes a custom Handler
+	// So HandlFunc takes a custom Handler
 	// Which is forcement takes into a reader and writer
 	// and then it will print whatever is written to the
 	// writer
 	http.HandleFunc("/", boardHandler)
-	http.ListenAndServe("0.0.0.0:8080", nil)
+	http.ListenAndServe("0.0.0.0:8080", context.ClearHandler(http.DefaultServeMux))
 }
