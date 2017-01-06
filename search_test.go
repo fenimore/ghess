af2da59f7ccb985df41088659cd44155e5ba6cec
polypmer <exorable.ludos@gmail.com>
2017-01-06 10:31:48 -0500

    Organize search tests

--- search_test.go
+++ search_test.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
-	//"github.com/polypmer/ghess"
 )
+
+// TODO: search for empassant
 
 func TestTension(t *testing.T) {
 	game := NewBoard()
@@ -15,6 +16,7 @@ func TestTension(t *testing.T) {
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	tension := game.Tension()
 	if tension[55] != 0 {
 		t.Error("d5 should be equally tense")
@@ -42,6 +44,7 @@ func TestSearchValid(t *testing.T) {
 	if err != nil {
 		t.Error("Fen error")
 	}
+
 	o, d = game.SearchValid()
 	exO = []int{85, 87}
 	exD = []int{75, 75}
@@ -55,56 +58,78 @@ func TestSearchValid(t *testing.T) {
 	if err != nil {
 		t.Error("Fen error")
 	}
+
 	o, d = game.SearchValid()
 	exO = []int{85}
 	exD = []int{76}
 	if !reflect.DeepEqual(o, exO) || !reflect.DeepEqual(d, exD) {
 		t.Error("3 Search doesn't return the correct/valid moves")
 	}
+}
 
-	// SearchValid finds Castling:
-	fen = `r3k2r/p6p/8/8/8/8/P6P/R3K2R w KQkq - 0 1`
+func TestSearchValidPawn(t *testing.T) {
+	var err error
+	game := NewBoard()
+	fen := `7k/pppppppp/8/8/8/8/PPPPPPPP/K7 w KQkq - 0 1`
 	err = game.LoadFen(fen)
 	if err != nil {
 		t.Error("Fen error")
 	}
-	o, d = game.SearchValid()
 
-	exO = []int{11, 11, 14, 14, 14, 14, 14, 14, 14, 18, 18, 18, 21, 21, 28, 28}
-	exD = []int{12, 13, 25, 23, 24, 15, 13, 18, 11, 17, 16, 15, 31, 41, 38, 48}
-	//exD = []int{12, 13, 13, 15, 23, 24, 25, 18, 11, 15, 16, 17, 31, 41, 38, 48}
+	o, d := game.SearchValid()
+	exO := []int{18, 21, 21, 22, 22, 23, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28}
+	exD := []int{17, 31, 41, 32, 42, 33, 43, 34, 44, 35, 45, 36, 46, 37, 47, 38, 48}
 	if !reflect.DeepEqual(o, exO) || !reflect.DeepEqual(d, exD) {
-		fmt.Println(o)
-		fmt.Println(d)
-		t.Error("4 Search doesn't find white castle")
+		t.Error("Search doesn't find White Pawn Moves")
 	}
+}
+
+func TestSearchValidCastle(t *testing.T) {
+	var err error
+	game := NewBoard()
+	// White to move
+	fen := `r3k2r/p6p/8/8/8/8/P6P/R3K2R w KQkq - 0 1`
+	err = game.LoadFen(fen)
+	if err != nil {
+		t.Error("Fen error")
+	}
+	o, d := game.SearchValid()
+
+	exO := []int{11, 11, 14, 14, 14, 14, 14, 14, 14, 18, 18, 18, 21, 21, 28, 28}
+	exD := []int{12, 13, 25, 23, 24, 15, 13, 18, 11, 17, 16, 15, 31, 41, 38, 48}
+	if !reflect.DeepEqual(o, exO) || !reflect.DeepEqual(d, exD) {
+		t.Error("Search doesn't find white castle")
+	}
+
 	// For black:
 	fen = `r3k2r/p6p/8/8/8/8/P6P/R3K2R b KQkq - 0 1`
 	err = game.LoadFen(fen)
 	if err != nil {
-
 		t.Error("Fen error")
 	}
+
 	o, d = game.SearchValid()
 	exO = []int{71, 71, 78, 78, 81, 81, 84, 84, 84, 84, 84, 84, 84, 88, 88, 88}
 	exD = []int{61, 51, 68, 58, 82, 83, 85, 73, 74, 75, 83, 88, 81, 87, 86, 85}
-	//exD = []int{51, 61, 58, 68, 82, 83, 73, 74, 75, 83, 85, 88, 81, 85, 86, 87}
 	if !reflect.DeepEqual(o, exO) || !reflect.DeepEqual(d, exD) {
-		fmt.Println(o)
-		fmt.Println(d)
-		t.Error("5 Search doesn't find Black castle")
-		// Doesn't find both black castles...
+		t.Error("Search doesn't find Black castle")
 	}
-	// Search for Pawn:
-	fen = `7k/pppppppp/8/8/8/8/PPPPPPPP/K7 w KQkq - 0 1`
-	err = game.LoadFen(fen)
-	if err != nil {
-		t.Error("Fen error")
-	}
+}
+
+func ExampleBoard_SearchValid() {
+	game := NewBoard()
+	o, d := game.SearchValid()
+	fmt.Println(o)
+	fmt.Println(d)
+	fen := "6k1/5p2/7p/1R1r4/P2P1R2/6P1/2r4K/8 w ---- - 0 42"
+	_ = game.LoadFen(fen)
 	o, d = game.SearchValid()
-	exO = []int{18, 21, 21, 22, 22, 23, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28}
-	exD = []int{17, 31, 41, 32, 42, 33, 43, 34, 44, 35, 45, 36, 46, 37, 47, 38, 48}
-	if !reflect.DeepEqual(o, exO) || !reflect.DeepEqual(d, exD) {
-		t.Error("7Search doesn't find White Pawn Moves")
-	}
+	fmt.Println(o)
+	fmt.Println(d)
+
+	// Output:
+	//[12 12 17 17 21 21 22 22 23 23 24 24 25 25 26 26 27 27 28 28]
+	//[33 31 38 36 31 41 32 42 33 43 34 44 35 45 36 46 37 47 38 48]
+	//[21 21 21 43]
+	//[31 11 12 23]
 }
